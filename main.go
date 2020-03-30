5b808b33ed5cb215de55fb7294ffa5753b0e0d82
Lee Archer <l.archer@scalr.com>
2020-03-30 11:37:24 +0300

    SCALRCORE-14832 Clean up filenames

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-tfe/tfe"
+	"github.com/scalr/terraform-provider-scalr/scalr"
 )
 
 func main() {
