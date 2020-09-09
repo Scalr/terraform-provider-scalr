ed0e30d9a9de2c3e4d0c62f28f0c498f3a103ce0
Nope <contact@dmytrechko.com>
2020-09-09 12:58:54 +0300

    SCALRCORE-16577 Add sensitive

--- scalr/resource_scalr_endpoint.go
+++ scalr/resource_scalr_endpoint.go
@@ -41,8 +41,9 @@ func resourceScalrEndpoint() *schema.Resource {
 			},
 
 			"secret_key": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:      schema.TypeString,
+				Required:  true,
+				Sensitive: true,
 			},
 
 			"timeout": {
