908ca01bd85f0d2e0276b5a563b57a7c5dc6be46
Vitalii Motso <vitalii.motso@gmail.com>
2020-10-07 13:53:48 +0300

    update

--- hello.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func hello() {
-	greeting := "Bob"
-	fmt.Printf("Hello, %s", greeting)
-}
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/scalr/terraform-provider-scalr/scalr"
 )
+import "fmt"
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: scalr.Provider})
+	greeting := "Bob"
+	fmt.Printf("Hello, %s", greeting)
 }
