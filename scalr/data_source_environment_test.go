16007de4cec0a0166d7f25a8bd4a161c546a560c
Vitalii Motso <vitalii.motso@gmail.com>
2020-10-26 16:00:57 +0200

    Add tests for data.scalr_environment
    IaCP Provider > Environment resource
    SCALRCORE-15443

---
+++ scalr/data_source_environment_test.go
@@ -0,0 +1,45 @@
+package scalr
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccEnvironmentDataSource_basic(t *testing.T) {
+	rInt := rand.New(rand.NewSource(time.Now().UnixNano())).Int()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEnvironmentDataSourceConfig(rInt),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.scalr_environment.test", "name", fmt.Sprintf("test-env-%d", rInt)),
+					resource.TestCheckResourceAttr("data.scalr_environment.test", "status", "Active"),
+					resource.TestCheckResourceAttr("data.scalr_environment.test", "account_id", "acc-svrcncgh453bi8g"),
+					resource.TestCheckResourceAttrSet("data.scalr_environment.test", "cost_estimation_enabled"),
+					resource.TestCheckResourceAttrSet("data.scalr_environment.test", "created_by.0.full_name"),
+					resource.TestCheckResourceAttrSet("data.scalr_environment.test", "created_by.0.email"),
+					resource.TestCheckResourceAttrSet("data.scalr_environment.test", "created_by.0.username"),
+				),
+			},
+		},
+	})
+}
+
+func testAccEnvironmentDataSourceConfig(rInt int) string {
+	return fmt.Sprintf(`
+resource "scalr_environment" "test" {
+  name       = "test-env-%d"
+  account_id = "acc-svrcncgh453bi8g"
+}
+
+data "scalr_environment" "test" {
+  id         = scalr_environment.test.id
+}`, rInt)
+}
