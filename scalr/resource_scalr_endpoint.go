8351c07e36c9f4a7cfbc3c776bc1f15dfe43c55f
Vitalii Motso <vitalii.motso@gmail.com>
2020-11-26 18:05:54 +0200

    Small fixes
    API > Publish Webhooks API into preview
    SCALRCORE-17105

--- scalr/resource_scalr_endpoint.go
+++ scalr/resource_scalr_endpoint.go
@@ -81,11 +81,14 @@ func resourceScalrEndpointCreate(d *schema.ResourceData, meta interface{}) error
 	// Create a new options struct.
 	options := scalr.EndpointCreateOptions{
 		Name:        scalr.String(name),
-		SecretKey:   scalr.String(d.Get("secret_key").(string)),
 		Url:         scalr.String(d.Get("url").(string)),
 		Environment: environment,
 		Account:     account,
 	}
+	if SecretKey, ok := d.GetOk("secret_key"); ok {
+		options.SecretKey = scalr.String(SecretKey.(string))
+	}
+	options.SecretKey = scalr.String(d.Get("secret_key").(string))
 
 	if maxAttempts, ok := d.GetOk("max_attempts"); ok {
 		options.MaxAttempts = scalr.Int(maxAttempts.(int))
