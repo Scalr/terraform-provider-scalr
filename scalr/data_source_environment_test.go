84b29e1c4d199b6742627954587fdb20500d14a6
Ivan Kovalkovskyi <vaniakov95@gmail.com>
2021-09-09 16:03:39 +0300

    Fix acc tests.

--- scalr/data_source_environment_test.go
+++ scalr/data_source_environment_test.go
@@ -31,7 +31,7 @@ func TestAccEnvironmentDataSource_basic(t *testing.T) {
 			},
 			{
 				Config:      testAccEnvironmentDataSourceNotFoundConfig(),
-				ExpectError: regexp.MustCompile("Environment env-123 not found"),
+				ExpectError: regexp.MustCompile("Environment with ID 'env-123' not found or user unauthorized"),
 				PlanOnly:    true,
 			},
 		},
