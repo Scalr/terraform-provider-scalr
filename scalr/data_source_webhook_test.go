98821af1f443ef4c0a198bf332d021dd2a57c5b8
Vladyslav Ovchynnykov <ovd4mail@gmail.com>
2022-07-20 13:56:06 +0300

    SCALRCORE-20119 Fixes testcase

--- scalr/data_source_webhook_test.go
+++ scalr/data_source_webhook_test.go
@@ -56,7 +56,7 @@ func TestAccWebhookDataSource_basic(t *testing.T) {
 				PlanOnly:    true,
 			},
 			{
-				Config:      testAccWebhookDataSourceNotFoundByNameConfig(rInt),
+				Config:      testAccWebhookDataSourceNotFoundByNameConfig(),
 				ExpectError: regexp.MustCompile("Webhook with name 'webhook-foo-bar-baz' not found or user unauthorized"),
 				PlanOnly:    true,
 			},
@@ -142,38 +142,12 @@ data scalr_webhook test {
 }`, rInt, defaultAccount)
 }
 
-func testAccWebhookDataSourceNotFoundByNameConfig(rInt int) string {
-	return fmt.Sprintf(`
-resource scalr_environment test {
-  name       = "test-env-%[1]d"
-  account_id = "%s"
-}
-
-resource scalr_workspace test {
-  name           = "test-ws-%[1]d"
-  environment_id = scalr_environment.test.id
-}
-
-resource scalr_endpoint test {
-  name           = "test endpoint-%[1]d"
-  timeout        = 15
-  max_attempts   = 3
-  url            = "https://example.com/webhook"
-  environment_id = scalr_environment.test.id
-}
-
-resource scalr_webhook test {
-  enabled      = false
-  name         = "test webhook-%[1]d"
-  events       = ["run:completed", "run:errored"]
-  endpoint_id  = scalr_endpoint.test.id
-  environment_id = scalr_environment.test.id
-}
-
+func testAccWebhookDataSourceNotFoundByNameConfig() string {
+	return `
 data scalr_webhook test {
   name           = "webhook-foo-bar-baz"
-  environment_id = scalr_environment.test.id
-}`, rInt, defaultAccount)
+  environment_id = "foobar"
+}`
 }
 
 func testAccWebhookNeitherNameNorIdSetConfig() string {
