1c95e32aec086f0d0dbf5b1f297c6c8faee7e64f
penja <a.penchuk@scalr.com>
2022-10-25 11:58:47 +0300

    SCALRCORE-23801 Provider > Policy group datasource has been broken

--- scalr/resource_scalr_module_test.go
+++ scalr/resource_scalr_module_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/scalr/go-scalr"
 )
 
 func TestAccScalrModule_basic(t *testing.T) {
