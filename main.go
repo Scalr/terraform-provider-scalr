1d8d41b45a9648d15f4f0ee75abc1fd85149af5c
Petro Protsakh <p.protsakh@scalr.com>
2022-11-14 12:27:21 +0200

    SCALRCORE-17414 Add debug mode flag

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"context"
+	"flag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/scalr/terraform-provider-scalr/scalr"
+	"log"
+	"os"
+)
+
+const (
+	scalrProviderName = "registry.scalr.io/scalr/scalr"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: scalr.Provider})
+	var debugMode bool
+	ctx := context.Background()
+
+	flag.BoolVar(&debugMode, "debug", false, "Start provider in debug mode.")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(ctx, scalrProviderName,
+			&plugin.ServeOpts{
+				ProviderFunc: scalr.Provider,
+			})
+		log.Printf("[ERROR] Could not start the debug server: %v", err)
+		os.Exit(1)
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: scalr.Provider})
+	}
 }
