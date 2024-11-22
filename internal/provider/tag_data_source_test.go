06bc06a7e4a2c0148c08ddc0d2b1df89e872909b
Petro Protsakh <p.protsakh@scalr.com>
2024-11-22 19:51:50 +0200

    SCALRCORE-26654 Rename datasource files

--- internal/provider/tag_data_source_test.go
+++ internal/provider/tag_data_source_test.go
@@ -12,8 +12,8 @@ func TestAccScalrTagDataSource_basic(t *testing.T) {
 	rInt := GetRandomInteger()
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
-		ProviderFactories: testAccProviderFactories,
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: protoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
 				Config:      `data scalr_tag test {}`,
