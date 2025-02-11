3aeb1401c6dd066cff189208c0d28c3c2e84aa1e
Denys Kolomytskyi <kolomytskyi@gmail.com>
2025-02-11 16:35:48 +0200

    SCALRCORE-32832 API > Environments > Cleanup EOL policy-groups relationship

--- examples/resources/scalr_environment/resource.tf
+++ examples/resources/scalr_environment/resource.tf
@@ -2,6 +2,5 @@ resource "scalr_environment" "test" {
   name                            = "test-env"
   account_id                      = "acc-xxxxxxxxxx"
   cost_estimation_enabled         = true
-  policy_groups                   = ["pgrp-xxxxxxxxxx", "pgrp-yyyyyyyyyy"]
   default_provider_configurations = ["pcfg-xxxxxxxxxx", "pcfg-yyyyyyyyyy"]
 }
