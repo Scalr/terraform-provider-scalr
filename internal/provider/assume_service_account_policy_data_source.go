5b226aebb13622b0d2ca4c95addae0768652e980
Vladyslav Mihun <mihunvlad@gmail.com>
2025-09-10 19:37:57 +0300

    SCALRCORE-35829 Scalr OIDC: implement logic of `like` operator (#461)
    * SCALRCORE-35829 Scalr OIDC: implement logic of `like` operator
    
    * SCALRCORE-35829

--- internal/provider/assume_service_account_policy_data_source.go
+++ internal/provider/assume_service_account_policy_data_source.go
@@ -92,7 +92,7 @@ func (d *assumeServiceAccountPolicyDataSource) Schema(_ context.Context, _ datas
 							Computed:    true,
 						},
 						"operator": schema.StringAttribute{
-							Description: "The operator to use for matching ('eq', 'contains', 'startswith', or 'endswith').",
+							Description: "The operator to use for matching ('eq', 'contains', 'startswith', 'like', or 'endswith').",
 							Computed:    true,
 						},
 					},
