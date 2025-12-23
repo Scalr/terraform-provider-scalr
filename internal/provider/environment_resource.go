f7c4d4fc43f0e0315d0d945d8b360a8edb58c4a1
soltyssss <y.soltys@scalr.com>
2025-12-23 13:47:42 +0200

    SCALRCORE-36474 - Add `scalr_federated_environments` resource to avoid circular dependencies

--- internal/provider/environment_resource.go
+++ internal/provider/environment_resource.go
@@ -439,8 +439,13 @@ func (r *environmentResource) Update(ctx context.Context, req resource.UpdateReq
 	if !plan.FederatedEnvironments.Equal(state.FederatedEnvironments) {
 		var planFederated []string
 		var stateFederated []string
-		resp.Diagnostics.Append(plan.FederatedEnvironments.ElementsAs(ctx, &planFederated, false)...)
-		resp.Diagnostics.Append(state.FederatedEnvironments.ElementsAs(ctx, &stateFederated, false)...)
+
+		if !plan.FederatedEnvironments.IsUnknown() && !plan.FederatedEnvironments.IsNull() {
+			resp.Diagnostics.Append(plan.FederatedEnvironments.ElementsAs(ctx, &planFederated, false)...)
+		}
+		if !state.FederatedEnvironments.IsUnknown() && !state.FederatedEnvironments.IsNull() {
+			resp.Diagnostics.Append(state.FederatedEnvironments.ElementsAs(ctx, &stateFederated, false)...)
+		}
 
 		opts.IsFederatedToAccount = ptr(false)
 
