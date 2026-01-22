da92427ca9ce80a512daef2bb707d805f6fdd89e
Petro Protsakh <p.protsakh@scalr.com>
2026-01-22 10:24:37 +0200

    SCALRCORE-37055 Allow importing workspace using name in Scalr provider (#484)
    * SCALRCORE-37055 Support importing scalr_workspace by env_name/ws_name
    
    * SCALRCORE-37055 Update changelog

--- examples/resources/scalr_workspace/import.sh
+++ examples/resources/scalr_workspace/import.sh
@@ -1,1 +1,5 @@
+# By ID:
 terraform import scalr_workspace.example ws-xxxxxxxxxx
+
+# By environment name + workspace name:
+terraform import scalr_workspace.example <environment-name>/<workspace-name>
